1d1064e8f50ded0d328da14738ea2ea0b0a40004
SMITHWILLSON <smithwillson43203@gmail.com>
2026-04-21 17:53:33 +0500

    added cpp practice files

--- vector.cpp
+++ vector.cpp
@@ -1,40 +1,64 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+//     vector<int> v;
+//     int num;
+
+//     cout << "Enter 5 numbers: ";
+//     for (int i = 0; i < 5; i++)
+//     {
+//         cin >> num;
+//         v.push_back(num);
+//     }
+
+//     cout << "\nNumbers: ";
+//     for (int x : v)
+//     {
+//         cout << x << " ";
+//     }
+
+//     int sum = 0;
+//     int largest = v[0];
+
+//     for (int x : v)
+//     {
+//         sum += x;
+
+//         if (x > largest)
+//         {
+//             largest = x;
+//         }
+//     }
+
+//     cout << "\nSum = " << sum;
+//     cout << "\nlargest = " << largest;
+
+//     return 0;
+// }
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main()
-{
+int main(){
     vector<int> v;
     int num;
 
     cout << "Enter 5 numbers: ";
-    for (int i = 0; i < 5; i++)
-    {
-        cin >> num;
+    for(int i= 0;i < 5;i++){
+        cin>>num;
         v.push_back(num);
     }
 
-    cout << "\nNumbers: ";
-    for (int x : v)
+    cout << "\nReverse order: ";
+    for (int i = v.size() - 1; i>= 0; i--)
     {
-        cout << x << " ";
+        cout << v[i] << " ";
     }
-
-    int sum = 0;
-    int largest = v[0];
-
-    for (int x : v)
-    {
-        sum += x;
-
-        if (x > largest)
-        {
-            largest = x;
-        }
-    }
-
-    cout << "\nSum = " << sum;
-    cout << "\nlargest = " << largest;
 
     return 0;
 }
