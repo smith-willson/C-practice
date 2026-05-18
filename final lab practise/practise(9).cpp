c54fb57c4712d4fd6ae89a4d9e767ad6bfc6e651
SMITHWILLSON <smithwillson43203@gmail.com>
2026-05-18 20:23:23 +0500

    added cpp practice files

--- final lab practise/practise(9).cpp
+++ final lab practise/practise(9).cpp
@@ -88,9 +88,11 @@ void login(User users[], int totalUsers)
 
     while (attempt > 0)
     {
-        cout<<endl;
+        cout << endl;
         cout << CYAN << "\nEnter Username: " << RESET;
-        cin >> userInput;
+        getline(cin, userInput);
+        while (userInput.empty())
+            getline(cin, userInput);
 
         cout << CYAN << "enter password: " << RESET;
         passInput = getPassword();
@@ -114,38 +116,82 @@ void login(User users[], int totalUsers)
         else
         {
             attempt--;
-cout << RED << "invalid username or password! " << RESET << endl;
-cout << YELLOW << attempt << " attempt(s) left" << RESET << endl;
+            cout << RED << "invalid username or password! " << RESET << endl;
+            cout << YELLOW << attempt << " attempt(s) left" << RESET << endl;
 
-if (attempt == 1)
-{
-    cout << YELLOW << "forgot password? (y/n): " << RESET;
-    char ans;
-    cin >> ans;
-    if (ans == 'y' || ans == 'Y')
-    {
-        cout << CYAN << "Enter Username to get Hint: " << RESET;
-        string hintUser;
-        cin >> hintUser;
-
-        bool hintfound = false;
-        for (int i = 0; i < totalUsers; i++)
-        {
-            if (hintUser == users[i].username)
+            if (attempt == 1)
             {
-                cout << YELLOW << "hint: " << users[i].hint << RESET << endl;
-                hintfound = true;
-                break;
+                cout << YELLOW << "forgot password? (y/n): " << RESET;
+                char ans;
+                cin >> ans;
+                cin.ignore();
+                if (ans == 'y' || ans == 'Y')
+                {
+                    cout << CYAN << "Enter Username to get Hint: " << RESET;
+                    string hintUser;
+                    getline(cin, hintUser);
+
+                    bool hintfound = false;
+                    for (int i = 0; i < totalUsers; i++)
+                    {
+                        if (hintUser == users[i].username)
+                        {
+                            cout << YELLOW << "hint: " << users[i].hint << RESET << endl;
+                            hintfound = true;
+                            break;
+                        }
+                    }
+                    if (!hintfound)
+                        cout << RED << "USERNAME NOT FOUND" << RESET << endl;
+                }
             }
-        }
-        if (!hintfound)
-            cout << RED << "USERNAME NOT FOUND" << RESET << endl;
-    }
-}
         }
     }
 
     cout << RED << BOLD << "\nACCESS DENIED! ACCOUNT LOCKED." << RESET << endl;
+}
+
+void registerUser(User users[], int &totalUsers)
+{
+    string newUsername, newPassword, newHint;
+
+    cout << endl;
+    cout << CYAN << "Enter New User: " << RESET;
+    getline(cin, newUsername);
+    while (newUsername.empty())
+        getline(cin, newUsername);
+    for (int i = 0; i < totalUsers; i++)
+    {
+        if (users[i].username == newUsername)
+        {
+            cout << RED << "username already exists!" << RESET << endl;
+            return;
+        }
+    }
+
+    cout << CYAN << "enter password: " << RESET;
+    newPassword = getPassword();
+
+    cout << CYAN << "Enter a hint for your password: " << RESET;
+    getline(cin, newHint);
+    while (newHint.empty())
+        getline(cin, newHint);
+
+    ofstream file("users.txt", ios::app);
+    file << newUsername << "," << newPassword << "," << newHint << endl;
+
+    if (file)
+    {
+        users[totalUsers].username = newUsername;
+        users[totalUsers].password = newPassword;
+        users[totalUsers].hint = newHint;
+        totalUsers++;
+        cout << GREEN << "Registration successful!" << RESET << endl;
+    }
+    else
+    {
+        cout << RED << "Failed to open users file." << RESET << endl;
+    }
 }
 int main()
 {
@@ -160,10 +206,11 @@ int main()
 
     int choice;
     cin >> choice;
+    cin.ignore();
     if (choice == 1)
         login(users, totalUsers);
     else if (choice == 2)
-        cout << YELLOW << "Register coming soon..." << RESET << endl;
+        registerUser(users, totalUsers);
     else
         cout << GREEN << "Goodbye!" << RESET << endl;
 
